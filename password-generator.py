c946b9814dd5ea8a4c8477f9e1202a86702281a0
dontcome4meb <dontcomeformeb@gmail.com>
2026-02-20 11:40:07 -0500

    Initial Setup

---
+++ password-generator.py
@@ -0,0 +1,9 @@
+# 1. Import necessary libraries
+
+# 2. Define the list of characters allowed in the password
+
+# 3. Ask the user for the password length
+
+# 4. Generate the password
+
+# 5. Print the result
